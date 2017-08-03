38006b7a399b98ece6f37f8b34d7ff29fce01e04
Octavio Mejia <arkord1@gmail.com>
2017-08-03 11:56:25 -0500

    Maps

--- src/pages/areas/list/areas.page.ts
+++ src/pages/areas/list/areas.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { AreasService } from '../areas.service';
 import { AreaPreviewPage } from '../preview/area-preview.page';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
 	templateUrl: 'areas.html'
@@ -11,12 +13,31 @@ export class AreasPage {
 	private AreasService: AreasService;
 	areas: any;
 
+	@ViewChild('map') mapElement: ElementRef;
+	map: any;
+
 	constructor(AreasService: AreasService, nav: NavController) {
 		this.AreasService = AreasService;
 		this.nav = nav;
 		this.AreasService.all().subscribe(
 			response => this.areas = response
 		)
+	}
+
+	ionViewDidLoad(){
+		this.loadMap();
+	}
+
+	loadMap() {
+		let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+ 
+		let mapOptions = {
+		center: latLng,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+	
+		this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 	}
 
 	goToArea(id) {
