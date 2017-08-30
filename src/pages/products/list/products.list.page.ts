b02849ac945341a0eb26332d7c83c2a35d811e89
Octavio <arkord1@gmail.com>
2017-08-29 19:19:48 -0500

    Products

--- src/pages/products/list/products.list.page.ts
+++ src/pages/products/list/products.list.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { ProductsService } from '../products.service';
 import { Product } from '../models/product.model';
@@ -13,12 +13,15 @@ import { ProductsItemPage } from '../item/products.item.page';
 export class ProductsListPage implements OnInit {
 	private service: ProductsService;
 	private nav: NavController;
+	private store: any;
 
 	public products: Product[];
 
-	constructor(service: ProductsService, nav: NavController) {
+	constructor(service: ProductsService, nav: NavController, navParams: NavParams) {
 		this.service = service;
 		this.nav = nav;
+		this.store = navParams.get("store_id");
+		alert(this.store);
 	}
 
 	ngOnInit(): void {
--- src/pages/products/stores/products.stores.page.ts
+++ src/pages/products/stores/products.stores.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductsListPage } from '../list/products.list.page';
 
 import { ProductsService } from '../products.service';
 
@@ -9,14 +10,18 @@ import { ProductsService } from '../products.service';
 })
 
 export class ProductsStoresPage {
+    private nav: NavController;
     private stores;
 
     constructor(ProductsService: ProductsService, nav: NavController) {
+        this.nav = nav;
         this.stores = ProductsService.getStores();
     }
     
     goProducts(id) {
-        alert(id);
+        this.nav.push(ProductsListPage, {
+            store_id: id
+        });
     }
 
 }
